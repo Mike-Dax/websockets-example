5d94de2510b980247b063e0fea0a7e976a34ce83
Michael Orenstein <michael@whiteshadows.me>
2021-01-06 12:33:25 +1030

    chore: Initial commit

---
+++ src/application/pages/DeviceLoadingPage.tsx
@@ -0,0 +1,6 @@
+import { DeviceLoadingScreen } from '@electricui/components-desktop-blueprint'
+import React from 'react'
+
+export const DeviceLoadingPage = () => {
+  return <DeviceLoadingScreen />
+}
